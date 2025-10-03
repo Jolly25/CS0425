318a94a6ce1c039e20879b5f9cd42af643a03a4b
Iris <88831643+Jolly25@users.noreply.github.com>
2025-10-03 12:52:44 +0200

    Update versioneCorretta.py

--- UNIT_1/S2/L5/versioneCorretta.py
+++ UNIT_1/S2/L5/versioneCorretta.py
@@ -3,17 +3,18 @@ import datetime
 def assistente_virtuale(comando):
     if comando == "Qual è la data di oggi?":
         oggi = datetime.date.today()
-        return "La data di oggi è " + oggi.strftime("%d/%m/%Y")
+        risposta = "La data di oggi è " + oggi.strftime("%d/%m/%Y")
 
     elif comando == "Che ore sono?":
         ora_attuale = datetime.datetime.now().time()
-        return "L'ora attuale è " + ora_attuale.strftime("%H:%M:%S")
+        risposta =  "L'ora attuale è " + ora_attuale.strftime("%H:%M:%S")
 
     elif comando == "Come ti chiami?":
-        return "Mi chiamo Assistente Virtuale"
+        risposta = "Mi chiamo Assistente Virtuale"
 
     else:
-        return "Non ho capito la tua domanda."
+        risposta = "Non ho capito la tua domanda."
+    return risposta 
 
 
 while True:
@@ -22,3 +23,4 @@ while True:
         print("Arrivederci!")
         break
     print(assistente_virtuale(comando_utente))
+
